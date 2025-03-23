7cbbc61393842c6c420fa6496bf7593a0eabf57f
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2025-03-22 17:45:01 -0700

    adjusted styling in latest payments display

--- src/components/pages/dashboard/PaymentDisplay.tsx
+++ src/components/pages/dashboard/PaymentDisplay.tsx
@@ -55,7 +55,7 @@ function PaymentDisplay() {
   }, [userData?.id ?? "guest"]);
 
   return (
-    <Card className="w-full">
+    <section className="w-full py-10">
       <div className="flex justify-between items-start">
         <div className="flex items-start gap-5">
           {/* LATEST HEADING WITH PAYMENTS COUNT */}
@@ -97,7 +97,7 @@ function PaymentDisplay() {
           />
         )}
       </div>
-    </Card>
+    </section>
   );
 }
 
