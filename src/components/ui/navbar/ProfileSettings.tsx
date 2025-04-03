cc3b66b9e990a1c2b908159f91ca3051ab2b4c6b
Jasmine Maduafokwa <jmaduafokwa@hotmail.com>
2025-04-03 19:40:19 +0100

    took out username input; almost forgot that there would be no usernames with this application

--- src/components/ui/navbar/ProfileSettings.tsx
+++ src/components/ui/navbar/ProfileSettings.tsx
@@ -48,29 +48,6 @@ function ProfileSettings({ user }: { readonly user: User | undefined}) {
       <div>
         <Accordion type="single" collapsible>
           <AccordionItem value="item-1">
-            <AccordionTrigger>Update username</AccordionTrigger>
-            <AccordionContent>
-              <form>
-                <Input htmlFor={"username"} label={"Username"} className="mt-3">
-                  <input className="form" id="username" name="username" />
-                </Input>
-                <div className="flex justify-end mt-4">
-                  <Submit
-                    loading={false}
-                    width_height="w-[85px] h-[40px]"
-                    width="w-[40px]"
-                    arrow_width_height="w-6 h-6"
-                    disabledLogic={true}
-                  />
-                </div>
-              </form>
-            </AccordionContent>
-          </AccordionItem>
-        </Accordion>
-      </div>
-      <div>
-        <Accordion type="single" collapsible>
-          <AccordionItem value="item-1">
             <AccordionTrigger>Update email</AccordionTrigger>
             <AccordionContent>
               <form>
